21469f5194bc5e3abdee6da0098b4cf5c7685a95
bl4ze4447 <belutoni06@gmail.com>
2026-01-11 04:01:00 +0200

    Remove unwanted error case in generate_file_info_group

--- src/ui/additional_info_panel.rs
+++ src/ui/additional_info_panel.rs
@@ -2,8 +2,7 @@ use crate::actions::select_action::SelectAction;
 use crate::icons_manager::IconsManager;
 use crate::lang_string::{LangKeys, LangString};
 use chrono::{DateTime, Local};
-use eframe::egui;
-use egui::{Image, Ui, Vec2};
+use egui::{Ui, Vec2};
 use std::path::Path;
 
 pub fn show(
@@ -32,65 +31,58 @@ fn generate_file_info_group(
     file: &Path,
     icons_manager: &IconsManager,
 ) {
-    match file.metadata() {
-        Ok(metadata) => {
-            if let Some(file_name) = file.file_name()
-                && let Some(file_name) = file_name.to_str()
-            {
+    if let Ok(metadata) = file.metadata() {
+        if let Some(file_name) = file.file_name()
+            && let Some(file_name) = file_name.to_str()
+        {
+            ui.group(|ui| {
+                ui.add(
+                    icons_manager
+                        .get_icon(&file.into())
+                        .clone()
+                        .fit_to_exact_size(Vec2::new(128.0, 128.0)),
+                );
+                ui.heading(file_name);
+
                 ui.group(|ui| {
-                    ui.add(
-                        icons_manager
-                            .get_icon(&file.into())
-                            .clone()
-                            .fit_to_exact_size(Vec2::new(128.0, 128.0)),
-                    );
-                    ui.heading(file_name);
+                    if let Some(extension) = file.extension()
+                        && let Some(extension) = extension.to_str()
+                        && metadata.is_file()
+                    {
+                        ui.label(lang_string.get(LangKeys::Extension) + extension);
+                    }
 
-                    ui.group(|ui| {
-                        if let Some(extension) = file.extension()
-                            && let Some(extension) = extension.to_str()
-                            && metadata.is_file()
-                        {
-                            ui.label(lang_string.get(LangKeys::Extension) + extension);
-                        }
+                    let bytes = metadata.len();
+                    if metadata.is_file() {
+                        ui.label(convert_bytes_size_to_human(bytes));
+                    }
 
-                        let bytes = metadata.len();
-                        if metadata.is_file() {
-                            ui.label(convert_bytes_size_to_human(bytes));
-                        }
+                    const DATE_TIME_FORMAT: &str = "%d/%m/%Y %T";
+                    let mut date_time: DateTime<Local>;
 
-                        const DATE_TIME_FORMAT: &str = "%d/%m/%Y %T";
-                        let mut date_time: DateTime<Local>;
-
-                        if let Ok(time) = metadata.created() {
-                            date_time = time.into();
-                            ui.label(
-                                lang_string.get(LangKeys::CreatedAt)
-                                    + date_time.format(DATE_TIME_FORMAT).to_string().as_str(),
-                            );
-                        }
-                        if let Ok(time) = metadata.accessed() {
-                            date_time = time.into();
-                            ui.label(
-                                lang_string.get(LangKeys::AccessedAt)
-                                    + date_time.format(DATE_TIME_FORMAT).to_string().as_str(),
-                            );
-                        }
-                        if let Ok(time) = metadata.modified() {
-                            date_time = time.into();
-                            ui.label(
-                                lang_string.get(LangKeys::ModifiedAt)
-                                    + date_time.format(DATE_TIME_FORMAT).to_string().as_str(),
-                            );
-                        }
-                    });
+                    if let Ok(time) = metadata.created() {
+                        date_time = time.into();
+                        ui.label(
+                            lang_string.get(LangKeys::CreatedAt)
+                                + date_time.format(DATE_TIME_FORMAT).to_string().as_str(),
+                        );
+                    }
+                    if let Ok(time) = metadata.accessed() {
+                        date_time = time.into();
+                        ui.label(
+                            lang_string.get(LangKeys::AccessedAt)
+                                + date_time.format(DATE_TIME_FORMAT).to_string().as_str(),
+                        );
+                    }
+                    if let Ok(time) = metadata.modified() {
+                        date_time = time.into();
+                        ui.label(
+                            lang_string.get(LangKeys::ModifiedAt)
+                                + date_time.format(DATE_TIME_FORMAT).to_string().as_str(),
+                        );
+                    }
                 });
-            }
-        }
-
-        Err(e) => {
-            // todo(bl4ze4447):
-            println!("{}", e);
+            });
         }
     }
 }
